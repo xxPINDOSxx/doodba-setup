5bbff2b10c69e2df1d8604e47b3031daa2f44b18
xxPINDOSxx <ljmnoonan@gmail.com>
2024-11-19 21:50:19 -0500

    .

--- doodba-setup.sh
+++ doodba-setup.sh
@@ -32,9 +32,8 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-
-
-
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 
 
